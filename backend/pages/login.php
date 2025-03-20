15b2e601964ca3d30e91431d15307f78ccc6c931
MiffysCode <your-email@example.com>
2025-03-21 03:05:11 +0600

    Updated login and register pages

--- backend/pages/login.php
+++ backend/pages/login.php
@@ -3,10 +3,10 @@
 <h2>Login</h2>
 <form action="../functions/user.php" method="POST">
     <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
+    <input type="email" id="email" name="email" required placeholder="example@email.com">
     
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
+    <label for="password">Password :</label>
+    <input type="text" id="password" name="password" required pattern="\d{5}" title="Password must be a 5-digit PIN">
     
     <input type="submit" name="login" value="Login">
 </form>
--- backend/pages/register.php
+++ backend/pages/register.php
@@ -2,15 +2,18 @@
 
 <h2>Register</h2>
 <form action="../functions/user.php" method="POST">
-    <label for="user_name">Name:</label>
-    <input type="text" id="user_name" name="user_name" required>
-    
+    <label for="first_name">First Name:</label>
+    <input type="text" id="first_name" name="first_name" required>
+
+    <label for="last_name">Last Name:</label>
+    <input type="text" id="last_name" name="last_name" required>
+
     <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
-    
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
-    
+    <input type="email" id="email" name="email" placeholder="example@example.com" required>
+
+    <label for="password">Password (5 digits):</label>
+    <input type="password" id="password" name="password" pattern="\d{5}" title="Please enter a 5-digit password" required>
+
     <input type="submit" name="register" value="Register">
 </form>
 
